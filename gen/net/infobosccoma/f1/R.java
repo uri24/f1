6d5127cacc17f3ee78f44f0d1533a8e8c14d21ca
Oriol <ocomas@infobosccoma.net>
2014-03-27 18:55:23 +0100

    he afegit 3 clases que es poden accedir des del menu i des de cadasquna de ellas pots tornar al menu

--- gen/net/infobosccoma/f1/R.java
+++ gen/net/infobosccoma/f1/R.java
@@ -41,12 +41,18 @@ public final class R {
         public static final int tnom=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_secon=0x7f030001;
+        public static final int activity_imatge=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_secon=0x7f030002;
+        public static final int activity_veu=0x7f030003;
+        public static final int activity_video=0x7f030004;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
-        public static final int secon=0x7f070001;
+        public static final int imatge=0x7f070000;
+        public static final int main=0x7f070001;
+        public static final int secon=0x7f070002;
+        public static final int veu=0x7f070003;
+        public static final int video=0x7f070004;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -55,7 +61,10 @@ public final class R {
         public static final int enom=0x7f050003;
         public static final int hello_world=0x7f050002;
         public static final int tcognom=0x7f050004;
+        public static final int title_activity_imatge=0x7f050008;
         public static final int title_activity_secon=0x7f050006;
+        public static final int title_activity_veu=0x7f050009;
+        public static final int title_activity_video=0x7f050007;
     }
     public static final class style {
         /** 
